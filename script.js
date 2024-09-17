823ff5eb2f6247558f862abfc53a46faa1424cd5
Roopa Chinchewadi <168030895+Roopa-Chinchewadi@users.noreply.github.com>
2024-09-17 20:32:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for additional interactions and animations can go here
+
+// Example: Initialize carousel
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 2000
+    });
+});
